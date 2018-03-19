a2b98eba5b94ba947a91582306e9dcdb7777ea88
sbxio <sbxio@outlook.com>
2018-03-18 22:05:25 -0400

    update(Home)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,6 +9,10 @@ export default class extends React.Component {
                 </header>
 
                 <p>Ready to jumpstart your blockchain education? Trying to figure out which course is the right fit for you or your developer teams?</p>
+                <div>
+                    <a href="#Courses">VIEW OUR COURSES</a>
+                    <div />
+                </div>
                 <p>The Blockchain Learning Group has facilitates comprehensive, hands-on courses aimed at the developer base - and the organizations that they belong to - and provides them with the knowledge, tools and confidence to make an immediate impact with blockchain projects.</p>
             </section>
         );
