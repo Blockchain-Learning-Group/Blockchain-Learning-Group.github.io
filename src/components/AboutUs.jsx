8a8b9ae19fb7e8fc77af3cce7e196879a3535158
sbxio <sbxio@outlook.com>
2018-03-22 19:38:31 -0400

    update(AboutUs+font)

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -11,19 +11,21 @@ export default class extends React.Component {
                         <p>We have developed a hands-on, immersive blockchain training course for developers that was created by our in-house developers, with a strong focus on smart contracts and decentralized application development.</p>
                     </header>
 
-                    <article>
-                        <h4>
-                            <strong>VISION</strong>
-                        </h4>
-                        <p>Our vision is a global community where developers and organizations attain a much stronger foundation of blockchain education in order to better serve their blockchain technology projects and goals.</p>
-                    </article>
+                    <footer>
+                        <article>
+                            <h4>
+                                <strong>VISION</strong>
+                            </h4>
+                            <p>Our vision is a global community where developers and organizations attain a much stronger foundation of blockchain education in order to better serve their blockchain technology projects and goals.</p>
+                        </article>
 
-                    <article>
-                        <h4>
-                            <strong>MISSION</strong>
-                        </h4>
-                        <p>Our mission is to provide a training course with the right mix of theoretical and practical components that empowers developers with an actionable knowledge base to help them make an immediate impact.</p>
-                    </article>
+                        <article>
+                            <h4>
+                                <strong>MISSION</strong>
+                            </h4>
+                            <p>Our mission is to provide a training course with the right mix of theoretical and practical components that empowers developers with an actionable knowledge base to help them make an immediate impact.</p>
+                        </article>
+                    </footer>
                 </div>
             </section>
         );
