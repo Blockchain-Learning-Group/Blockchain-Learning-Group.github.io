4824cb7319dc8d934d2dda6dabb7ce4d9366d372
sbxio <sbxio@outlook.com>
2018-03-22 11:40:15 -0400

    update(Courses): as per copy

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -6,7 +6,7 @@ export default class extends React.Component {
             <section id="Courses">
                 <header>
                     <h3>Our Courses</h3>
-                    <p>Aligned with our vision to focus our efforts at the developer community - and the organizations they are affiliated with - we offer two specific education tracks</p>
+                    <p>Aligned with our vision to focus our efforts on the developer community - and the organizations they are affiliated with - we offer two specific education tracks:</p>
 
                     <ul>
                         <li className="mdc-elevation--z16">
@@ -32,7 +32,7 @@ export default class extends React.Component {
                                 </div>
                             </header>
                             <div>
-                                <p>For organizations who want to both train their developers and immediately turn this knowledge into action, the Blockchain Learning Group provides a comprehensive package to jumpstart your blockchain goals. Please contact us for more details on the business packages.</p>
+                                <p>For organizations who want to both train their developers and immediately turn this knowledge into action, the Blockchain Learning Group provides a comprehensive package to jumpstart your blockchain strategies into action. Please contact us for more details on our business packages.</p>
                             </div>
                             <footer>
                                 <a href="#CONTACTUS">CONTACT US</a>
@@ -77,11 +77,11 @@ export default class extends React.Component {
                                     <ul>
                                         <li>Enthusiasm and a genuine passion for Blockchain technology</li>
                                         <li>Software development experience is a plus</li>
-                                        <li>Laptop, Ubuntu preferred,  Mac / Windows OK</li>
+                                        <li>Laptop, Ubuntu preferred, Mac / Windows OK</li>
                                     </ul>
 
                                     <h5>OUTCOMES</h5>
-                                    <p>All participants will gain a deep understanding of Blockchain technology, how it works, the ever growing landscape, cryptocurrencies and ICOs, and most importantly, hands-on experience with full stack DApp development.  Participants will have successfully built, tested and deployed several smart contracts as well as a full stack DApp.</p>
+                                    <p>All participants will gain a deep understanding of Blockchain technology, how it works, the ever growing landscape, cryptocurrencies and ICOs, and most importantly, hands-on experience with full stack DApp development. Participants will have successfully built, tested and deployed several smart contracts as well as a full stack DApp.</p>
                                 </footer>
                             </div>
                         </li>
