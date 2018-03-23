677b136f8e63072307b165a80377b9595930a6ce
sbxio <sbxio@outlook.com>
2018-03-22 22:04:03 -0400

    update(nav-header)

--- src/App.jsx
+++ src/App.jsx
@@ -24,35 +24,37 @@ export default class extends React.Component {
         return (
             <div id="App">
                 <header className="mdc-elevation--z8">
-                    <div className="logo" />
+                    <div className="--blg-wrapper">
+                        <div className="logo" />
 
-                    <div className="menu">
-                        <i className="material-icons mdc-toolbar__menu-icon" onClick={this.showMenuList.bind(this)}>menu</i>
+                        <div className="menu">
+                            <i className="material-icons mdc-toolbar__menu-icon" onClick={this.showMenuList.bind(this)}>menu</i>
+                        </div>
+
+                        <ul ref={(el) => this.menu_list = el} onClick={this.hideMenuList.bind(this)}>
+                            <li>
+                                <a href="#Home">Home</a>
+                            </li>
+                            <li>
+                                <a href="#AboutUs">About Us</a>
+                            </li>
+                            <li>
+                                <a href="#Courses">Courses</a>
+                            </li>
+                            <li>
+                                <a href="#Team">Our Team</a>
+                            </li>
+                            <li>
+                                <a href="#AdvisoryBoard">Advisory Board</a>
+                            </li>
+                            <li>
+                                <a href="#OurClients">Our Clients</a>
+                            </li>
+                            <li>
+                                <a href="ContactUs">Contact Us</a>
+                            </li>
+                        </ul>
                     </div>
-
-                    <ul ref={(el) => this.menu_list = el} onClick={this.hideMenuList.bind(this)}>
-                        <li>
-                            <a href="#Home">Home</a>
-                        </li>
-                        <li>
-                            <a href="#AboutUs">About Us</a>
-                        </li>
-                        <li>
-                            <a href="#Courses">Courses</a>
-                        </li>
-                        <li>
-                            <a href="#Team">Our Team</a>
-                        </li>
-                        <li>
-                            <a href="#AdvisoryBoard">Advisory Board</a>
-                        </li>
-                        <li>
-                            <a href="#OurClients">Our Clients</a>
-                        </li>
-                        <li>
-                            <a href="ContactUs">Contact Us</a>
-                        </li>
-                    </ul>
                 </header>
 
                 <main>
