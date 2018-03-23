1b4be61edd9904169aa3d3b5181e61561cdefc21
sbxio <sbxio@outlook.com>
2018-03-22 22:46:38 -0400

    fix(nav-header+layout)

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import AboutUs from './components/AboutUs';
 import AdvisoryBoard from './components/AdvisoryBoard';
 import Clients from './components/Clients';
+import ContactUs from './components/ContactUs';
 import Courses from './components/Courses';
 import Diversity from './components/Diversity';
 import Home from './components/Home';
@@ -58,36 +59,18 @@ export default class extends React.Component {
                 </header>
 
                 <main>
-                    <Home />
-                    <AboutUs />
-                    <Courses />
-                    <Diversity />
-                    <Clients />
-                    <Team />
-                    <AdvisoryBoard />
-                </main>
-
-                <footer>
-                    <address>
-                        <strong>Where we are located</strong>
-                        <div>Blockchain Learning Group Inc.</div>
-                        <div>24 Murray Spiers Court, Markham, ON, L6C 3C5 CANADA</div>
-                        <div style={{ marginTop: '1rem' }}>Let's talk</div>
-                        <div>
-                            <a href="tel:+14165008518">+1 (416) 500-8518</a>
-                        </div>
-                        <div>
-                            <a href="mailto:murtaza@blockchainlearninggroup.com">murtaza@BlockchainLearningGroup.com</a>
-                        </div>
-                    </address>
-                    <div className="social">
-                        <a target="_blank" rel="noopener noreferrer" className="linkedin" href="https://ca.linkedin.com/company/blockchainlearninggroup"></a>
-                        <a target="_blank" rel="noopener noreferrer" className="twitter" href="https://twitter.com/BlockchainLG"></a>
-                        <a target="_blank" rel="noopener noreferrer" className="github" href="https://github.com/Blockchain-Learning-Group"></a>
-                        <a target="_blank" rel="noopener noreferrer" className="slack" href="https://blockchainlearning.slack.com"></a>
+                    <div className="blg--main-wrapper">
+                        <Home />
+                        <AboutUs />
+                        <Courses />
+                        <Diversity />
+                        <Clients />
+                        <Team />
+                        <AdvisoryBoard />
+                        <ContactUs />
                     </div>
-                </footer>
-            </div >
+                </main>
+            </div>
         );
     }
 }
---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default class extends React.Component {
+    render() {
+        return (
+            <section id="ContactUs">
+                <address>
+                    <strong>Where we are located</strong>
+                    <div>Blockchain Learning Group Inc.</div>
+                    <div>24 Murray Spiers Court, Markham, ON, L6C 3C5 CANADA</div>
+                    <div style={{ marginTop: '1rem' }}>Let's talk</div>
+                    <div>
+                        <a href="tel:+14165008518">+1 (416) 500-8518</a>
+                    </div>
+                    <div>
+                        <a href="mailto:murtaza@blockchainlearninggroup.com">murtaza@BlockchainLearningGroup.com</a>
+                    </div>
+                </address>
+                <div className="social">
+                    <a target="_blank" rel="noopener noreferrer" className="linkedin" href="https://ca.linkedin.com/company/blockchainlearninggroup"></a>
+                    <a target="_blank" rel="noopener noreferrer" className="twitter" href="https://twitter.com/BlockchainLG"></a>
+                    <a target="_blank" rel="noopener noreferrer" className="github" href="https://github.com/Blockchain-Learning-Group"></a>
+                    <a target="_blank" rel="noopener noreferrer" className="slack" href="https://blockchainlearning.slack.com"></a>
+                </div>
+            </section>
+        );
+    }
+}
