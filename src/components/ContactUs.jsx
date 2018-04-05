b24217aa6eb19485546f1db28fc2e8c5aee55fb4
sbxio <sbxio@outlook.com>
2018-04-04 22:43:53 -0400

    fix(footer)

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -8,15 +8,11 @@ export default class extends React.Component {
                     <div>Looking to find out more information or how <strong><a href="https://BlockscaleSolutions.com">Blockscale Solutions</a></strong> - or <strong><a href="https://BlockchainLearningGroup.com">Blockchain Learning Group</a></strong> - can help transform your strategies into action, please contact us at</div>
                     <div className="logos">
                         <div>
-                            <img alt="Blockscale Solutions" src="images/BlockscaleSolutions.png" style={{ height: '50px', padding: '1.618rem 1rem 0.618rem' }} />
-                        </div>
-                        <div>
                             <img alt="Blockchain Learning Group" src="images/BlockchainLearningGroup.png" style={{ height: '70px', padding: '0.618rem 1rem' }} />
                         </div>
-                    </div>
-                    <div style={{ marginTop: '1rem' }}>Let's talk</div>
-                    <div>
-                        <a href="tel:+14165008518">+1 (416) 500-8518</a>
+                        <div>
+                            <img alt="Blockscale Solutions" src="images/BlockscaleSolutions.png" style={{ height: '50px', padding: '1.618rem 1rem 0.618rem' }} />
+                        </div>
                     </div>
                 </address>
                 <div className="social">
