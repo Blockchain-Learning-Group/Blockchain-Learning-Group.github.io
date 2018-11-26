6a41ac2081eb1438c557cbb8d7f00c5135ed4941
Adam J Lemmon <adamjlemmon@gmail.com>
2018-11-26 13:25:33 -0500

    update routing for prod

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ export default class extends React.Component {
     render() {
         return (
             <div id="App">
-                <Route exact={true} path="/" render={() => <Main />} />
-                <Route exact={true} path="/challenge-series" render={() => <ChallengeSeries />} />
+                <Route exact={true} path={process.env.PUBLIC_URL + "/"} render={() => <Main />} />
+                <Route exact={true} path={process.env.PUBLIC_URL + "/challenge-series"} render={() => <ChallengeSeries />} />
             </div>
         );
     }
