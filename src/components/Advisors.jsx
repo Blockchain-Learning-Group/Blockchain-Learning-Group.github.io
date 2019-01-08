8613f8acf3d98a3326fc938750cbe37b37180438
sbxio <sbxio@outlook.com>
2019-01-08 23:19:32 +0000

    dev: removed Lincoln Teo

--- src/components/Advisors.jsx
+++ src/components/Advisors.jsx
@@ -7,12 +7,9 @@ export default class extends React.Component {
                 <header>
                     <h3>Advisors</h3>
                     <p>
-                        We've aligned with leading blockchain experts worldwide
-                        and the developer community at large to form our
-                        advisory board, and we are just starting! We will
-                        continue to add thought leaders, corporate veterans and
-                        technology guru's on as advisors as we continue our
-                        journey.
+                        We've aligned with leading blockchain experts worldwide and the developer community at large to
+                        form our advisory board, and we are just starting! We will continue to add thought leaders,
+                        corporate veterans and technology guru's on as advisors as we continue our journey.
                     </p>
                 </header>
 
@@ -21,15 +18,9 @@ export default class extends React.Component {
                         <header>
                             <div>
                                 <span className="avatar">
-                                    <img
-                                        alt=""
-                                        src="images/profiles/Leanne.jpg"
-                                    />
+                                    <img alt="" src="images/profiles/Leanne.jpg" />
                                 </span>
-                                <span
-                                    className="linkedin"
-                                    style={{ display: "none" }}
-                                >
+                                <span className="linkedin" style={{display: "none"}}>
                                     <a
                                         target="_blank"
                                         rel="noopener noreferrer"
@@ -44,38 +35,24 @@ export default class extends React.Component {
                                 <div>Leanne Kemp</div>
                             </h4>
                             <aside>
+                                <p>Leanne Kemp is the Founder & CEO of Everledger.</p>
                                 <p>
-                                    Leanne Kemp is the Founder & CEO of
-                                    Everledger.
+                                    Using her extensive background in emerging technology, business, jewellery and
+                                    insurance, Leanne is pushing boundaries with Everledger in building a global
+                                    verification system that asserts transparency along the entire supply chain process.
+                                    With a rich history in innovation, Leanne previously founded three Australian
+                                    startups where she introduced and patented transformational technology ideas
+                                    alongside various solutions to streamline inventory and supply chain operations.
                                 </p>
                                 <p>
-                                    Using her extensive background in emerging
-                                    technology, business, jewellery and
-                                    insurance, Leanne is pushing boundaries with
-                                    Everledger in building a global verification
-                                    system that asserts transparency along the
-                                    entire supply chain process. With a rich
-                                    history in innovation, Leanne previously
-                                    founded three Australian startups where she
-                                    introduced and patented transformational
-                                    technology ideas alongside various solutions
-                                    to streamline inventory and supply chain
-                                    operations.
+                                    Her leadership role in technology has been recognised, winning awards including
+                                    Innovator of the Year 2018 at the Women in IT Awards (London). She has also been
+                                    named an IBM Champion for 2018.
                                 </p>
                                 <p>
-                                    Her leadership role in technology has been
-                                    recognised, winning awards including
-                                    Innovator of the Year 2018 at the Women in
-                                    IT Awards (London). She has also been named
-                                    an IBM Champion for 2018.
-                                </p>
-                                <p>
-                                    Leanne is an appointed member of the World
-                                    Economic Forum’s Global Blockchain Business
-                                    Council and a Co-Chair for the World Trade
-                                    Board's Sustainable Trade Action Group. She
-                                    is also on the IBM Blockchain Platform Board
-                                    of Advisors.
+                                    Leanne is an appointed member of the World Economic Forum’s Global Blockchain
+                                    Business Council and a Co-Chair for the World Trade Board's Sustainable Trade Action
+                                    Group. She is also on the IBM Blockchain Platform Board of Advisors.
                                 </p>
                             </aside>
                         </footer>
@@ -85,15 +62,9 @@ export default class extends React.Component {
                         <header>
                             <div>
                                 <span className="avatar">
-                                    <img
-                                        alt=""
-                                        src="images/profiles/Tony.jpg"
-                                    />
+                                    <img alt="" src="images/profiles/Tony.jpg" />
                                 </span>
-                                <span
-                                    className="linkedin"
-                                    style={{ display: "none" }}
-                                >
+                                <span className="linkedin" style={{display: "none"}}>
                                     <a
                                         target="_blank"
                                         rel="noopener noreferrer"
@@ -108,35 +79,23 @@ export default class extends React.Component {
                                 <div>Tony Gaffney</div>
                             </h4>
                             <aside>
+                                <p>Tony is Managing Partner, Board and CEO Services at Odgers Berndtson Canada.</p>
                                 <p>
-                                    Tony is Managing Partner, Board and CEO
-                                    Services at Odgers Berndtson Canada.
-                                </p>
-                                <p>
-                                    He has a proven record of creating value, in
-                                    diverse industries and geographies by;
-                                    setting direction, igniting growth, leading
-                                    change, driving performance, servicing
+                                    He has a proven record of creating value, in diverse industries and geographies by;
+                                    setting direction, igniting growth, leading change, driving performance, servicing
                                     clients and empowering employees.
                                 </p>
                                 <p>
-                                    Tony is particularly adept at capitalizing
-                                    on digital and technology disruptions, both
-                                    as a market disrupter and industry
-                                    incumbent. He was most recently CEO of Aon
-                                    Hewitt, Canada. Before joining Aon Hewitt,
-                                    Tony was a Managing Partner at Accenture,
-                                    and has served as the President and CEO of
-                                    Bell Nexxia and as the CEO of BCE Emergis.
-                                    He has also held international leadership
-                                    positions with MCI Telecommunications, SHL
-                                    Systemhouse Inc., and Andersen Consulting.
+                                    Tony is particularly adept at capitalizing on digital and technology disruptions,
+                                    both as a market disrupter and industry incumbent. He was most recently CEO of Aon
+                                    Hewitt, Canada. Before joining Aon Hewitt, Tony was a Managing Partner at Accenture,
+                                    and has served as the President and CEO of Bell Nexxia and as the CEO of BCE
+                                    Emergis. He has also held international leadership positions with MCI
+                                    Telecommunications, SHL Systemhouse Inc., and Andersen Consulting.
                                 </p>
                                 <p>
-                                    Tony holds a Bachelor of Engineering
-                                    (B.A.I.) degree and M.A. from Trinity
-                                    College Dublin, and is a graduate of the
-                                    Rotman Corporate Directors program (ICD.D).
+                                    Tony holds a Bachelor of Engineering (B.A.I.) degree and M.A. from Trinity College
+                                    Dublin, and is a graduate of the Rotman Corporate Directors program (ICD.D).
                                 </p>
                             </aside>
                         </footer>
@@ -146,15 +105,9 @@ export default class extends React.Component {
                         <header>
                             <div>
                                 <span className="avatar">
-                                    <img
-                                        alt=""
-                                        src="images/profiles/Jonathan.jpg"
-                                    />
+                                    <img alt="" src="images/profiles/Jonathan.jpg" />
                                 </span>
-                                <span
-                                    className="linkedin"
-                                    style={{ display: "none" }}
-                                >
+                                <span className="linkedin" style={{display: "none"}}>
                                     <a
                                         target="_blank"
                                         rel="noopener noreferrer"
@@ -170,22 +123,14 @@ export default class extends React.Component {
                             </h4>
                             <aside>
                                 <p>
-                                    Currently the Chief Information Officer
-                                    (CIO) for the City of Palo Alto, is a
-                                    multiple award-winning technology leader
-                                    whose 25-year career has spanned both the
-                                    private and public sectors. In 2017, he was
-                                    named one of the top 100 CIOs in the world
-                                    and in 2016 he was named one of the top 20
-                                    most influential CIO's in the United States.
-                                    Dr. Reichental is also recognized as a
-                                    global thought leader on a number of
-                                    emerging trends including urban innovation
-                                    and blockchain technology. Dr. Reichental
-                                    works with his teams to apply technology
-                                    innovation in organizations to create new
-                                    value and to enable work to be more
-                                    meaningful and fun.
+                                    Currently the Chief Information Officer (CIO) for the City of Palo Alto, is a
+                                    multiple award-winning technology leader whose 25-year career has spanned both the
+                                    private and public sectors. In 2017, he was named one of the top 100 CIOs in the
+                                    world and in 2016 he was named one of the top 20 most influential CIO's in the
+                                    United States. Dr. Reichental is also recognized as a global thought leader on a
+                                    number of emerging trends including urban innovation and blockchain technology. Dr.
+                                    Reichental works with his teams to apply technology innovation in organizations to
+                                    create new value and to enable work to be more meaningful and fun.
                                 </p>
                             </aside>
                         </footer>
@@ -194,14 +139,8 @@ export default class extends React.Component {
                     <li>
                         <header>
                             <div>
-                                <span
-                                    className="avatar"
-                                    style={{ backgroundColor: "initial" }}
-                                />
-                                <span
-                                    className="linkedin"
-                                    style={{ display: "none" }}
-                                >
+                                <span className="avatar" style={{backgroundColor: "initial"}} />
+                                <span className="linkedin" style={{display: "none"}}>
                                     <a
                                         target="_blank"
                                         rel="noopener noreferrer"
@@ -213,71 +152,15 @@ export default class extends React.Component {
                         </header>
                         <footer>
                             <h4>
-                                <div>HG</div>
+                                <div>Dave Mowat</div>
                             </h4>
                             <aside>
                                 <p>
-                                    HG, the CEO and a founder of BlockRe, is one
-                                    of the leaders in the enterprise blockchain
-                                    space who was the first dedicated blockchain
-                                    hire at two of the largest global banks. HG
-                                    is a serial entrepreneur with experience and
-                                    expertise across multiple domains, which has
-                                    allowed him to bring a unique perspective to
-                                    solving the challenges of safely operating
-                                    cryptocurrency and blockchain
-                                    infrastructure.
-                                </p>
-                            </aside>
-                        </footer>
-                    </li>
-
-                    <li>
-                        <header>
-                            <div>
-                                <span className="avatar">
-                                    <img
-                                        alt=""
-                                        src="images/profiles/Lincoln.jpg"
-                                    />
-                                </span>
-                                <span
-                                    className="linkedin"
-                                    style={{ display: "none" }}
-                                >
-                                    <a
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="linkedin"
-                                        href="https://ca.linkedin.com/company/blockchainlearninggroup"
-                                    />
-                                </span>
-                            </div>
-                        </header>
-                        <footer>
-                            <h4>
-                                <div>Lincoln Teo</div>
-                            </h4>
-                            <aside>
-                                <p>
-                                    Currently an Advisor with the central bank
-                                    of Brunei Darussalam. He has managed
-                                    multiple credit bureau projects in
-                                    South-East Asia (Singapore, Malaysia,
-                                    Indonesia, Vietnam and Brunei) as a senior
-                                    executive of Dun & Bradstreet and then with
-                                    Experian.
-                                </p>
-                                <p>
-                                    Lincoln’s experience includes serving on
-                                    project teams of the World Bank Group and
-                                    the Asian Development Bank. More recently,
-                                    he founded Intel Wise, an ICT firm that has
-                                    successfully integrated information
-                                    registries containing data on more than 80+
-                                    million companies in China and also
-                                    expanding the use of blockchain technologies
-                                    to link corporate registries regionally.
+                                    HG, the CEO and a founder of BlockRe, is one of the leaders in the enterprise
+                                    blockchain space who was the first dedicated blockchain hire at two of the largest
+                                    global banks. HG is a serial entrepreneur with experience and expertise across
+                                    multiple domains, which has allowed him to bring a unique perspective to solving the
+                                    challenges of safely operating cryptocurrency and blockchain infrastructure.
                                 </p>
                             </aside>
                         </footer>
